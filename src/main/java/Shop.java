39d41d020a3106f9dda1407abba052ec8e86c0cd
Mark <nirvanafoo86@hotmail.com>
2019-05-17 20:29:25 +0100

    Creating Test files

--- src/main/java/Shop.java
+++ src/main/java/Shop.java
@@ -1,2 +1,15 @@
+import Instruments.Instrument;
+
+import java.util.ArrayList;
+
 public class Shop {
+
+    private String name;
+    private ArrayList<Instrument> instruments;
+    private Till till;
+
+
+    public Shop(String name){
+        this.name = name;
+    }
 }
